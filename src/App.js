01500f9c29f839f82beafede17d30aa13d67d46e
Haley Tate <haleytate@Haleys-MacBook-Pro.local>
2019-02-25 16:47:39 -0700

    build static page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <headerStyles className="App-header">
+        <h1>Radio Mirror</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Type or paste your speech in the box below.<br/> 
+            Press play to hear how it sounds, stop to stop, <br/> 
+            and save to save desirable outputs.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        </headerStyles>
+        <formStyles>
+        <form>
+          <textarea 
+            rows="20"
+            cols="50"
           >
-            Learn React
-          </a>
-        </header>
+            Text here... 
+          </textarea>
+          <br/>
+          <button>Play</button>
+          <button>Stop</button>
+          <button>Save</button>
+        </form>
+        <br/>
+        <p>Saved Speeches</p>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+        </formStyles>
       </div>
     );
   }
 }
 
+const formStyles = styled.div`
+  margin: 30px 30px 30px 30px;
+`
+
+const headerStyles = styled.header`
+  background-color: #282c34;
+  text-align: center;
+  font-size: calc(18px + 2vmin);
+  color: white;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 export default App;
